ce190060ac91a4187ea542a398d507eb3b7f8487
Raymond Xu <rx67@cornell.edu>
2025-11-13 17:01:28 -0500

    remove things in events

--- src/pages/events/Events.tsx
+++ src/pages/events/Events.tsx
@@ -1,23 +1,9 @@
 import './Events.css';
-import EventCard from './EventCard';
-import { eventsData, upcomingEventData } from './events.data';
-import UpcomingEventCard from './UpcomingEventCard';
 
 export default function Events() {
   return (
     <div>
       <h1>Events</h1>
-      <div className="upcoming-events-container">
-        {upcomingEventData.map((upcomingEventData) => (
-          <UpcomingEventCard key={upcomingEventData.title} upcomingEventData={upcomingEventData} />
-        ))}
-      </div>
-      
-      <div className="events-container">
-        {eventsData.map((eventData) => (
-          <EventCard key={eventData.title} eventData={eventData} />
-        ))}
-      </div>
     </div>
   );
 }

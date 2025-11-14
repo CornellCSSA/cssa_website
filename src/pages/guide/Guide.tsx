10d760c3f304d4c5cdb66a553fe4983f23cfd6f4
Raymond Xu <rx67@cornell.edu>
2025-11-14 01:09:08 -0500

    complete Guide page

--- src/pages/guide/Guide.tsx
+++ src/pages/guide/Guide.tsx
@@ -1,9 +1,16 @@
 import './Guide.css';
+import HeroSection from '../../components/HeroSection';
+import guideHeroImage from '../../assets/guide-hero.jpg';
+import guideCover from '../../assets/guide.jpg';
 
 export default function Guide() {
   return (
     <div>
-      <h1>Guide</h1>
+      <HeroSection heroImage={guideHeroImage} title="康村指南" />
+      <div className="guide-container">
+        <h2 className="guide-heading">新生手册</h2>
+        <img src={guideCover} alt="Guide Cover" className="guide-cover-img" />
+      </div>
     </div>
   );
 }
